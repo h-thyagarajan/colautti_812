f4be4bafcf6d85a3da5039e6f0793809b1db5722
h-thyagarajan <harshavardhanthyagarajan@gmail.com>
2023-01-10 17:06:23 -0500

    Commit 7 - nearly done, but fix relative paths?

--- scripts/2VolumeEstimato.R
+++ scripts/2VolumeEstimato.R
@@ -1,5 +1,5 @@
 #Write an R script called volumeEstimato.R that will read a file called  measurements.csv
-measurements <- read.csv(file = "data/measurements.csv")
+measurements <- read.csv(file = "/Users/ht/0/Stats/2 Colautti_812/trial_repo/data/measurements.csv")
 
 #Getting rid of the extra indexing column, when the file is recalled
 if (names(measurements)[1] == "X") {measurements <- measurements[,-1]}
@@ -9,4 +9,4 @@ volume <- function(l,w) {v <- pi*(w/2)^2*l; return(v)}
 
 #Over-write the original measurements.csv file so that it now contains a Volume column.
 measurements$Limb.Volume <- volume(measurements$Limb.Length, measurements$Limb.Width)
-write.csv(measurements, file = "data/measurements1.csv")
+write.csv(measurements, file = "/Users/ht/0/Stats/2 Colautti_812/trial_repo/data/measurements1.csv")
--- scripts/3Summary&Visualization.R
+++ scripts/3Summary&Visualization.R
@@ -1,5 +1,5 @@
 #Load the revised measurements.csv
-measurements <- read.csv(file = "data/measurements.csv")
+measurements <- read.csv(file = "/Users/ht/0/Stats/2 Colautti_812/trial_repo/data/measurements.csv")
 #Getting rid of the extra indexing column, when the file is recalled
 if (names(measurements)[1] == "X") {measurements <- measurements[,-1]}
 
@@ -7,7 +7,7 @@ if (names(measurements)[1] == "X") {measurements <- measurements[,-1]}
 require("tidyverse")
 measurements <- data.frame(measurements %>% arrange(Species, Observer, Limb.Volume))
 measurements$Species <- as.factor(measurements$Species)
-write.csv(measurements, file = "data/measurements.csv")
+write.csv(measurements, file = "/Users/ht/0/Stats/2 Colautti_812/trial_repo/data/measurements.csv")
 
 #Create a table showing the average volume for each species
 measurements.summary <- data.frame(measurements %>% group_by(Species) %>% summarise(Mean.Volume=mean(Limb.Volume), StdDev.Volume=sd(Limb.Volume), Count=n()))
@@ -19,9 +19,10 @@ observer.summary <- data.frame(measurements %>% group_by(Observer) %>% summarise
 #Create a single box plot (or similar) to compare key features of the 
 #distributions of volumes for each species. 
 #See <http://www.sthda.com/english/wiki/ggplot2-box-plot-quick-start-guide-r-software-and-data-visualization>
-b1 <- boxplot(Limb.Volume~Species, data=measurements)
+
+#b1 <- boxplot(Limb.Volume~Species, data=measurements)
 #or
-b2 <- ggplot(measurements, aes(x=Species, y=Limb.Volume)) + geom_boxplot() + theme_pub() + theme(axis.text.x = element_text(angle = 25, vjust = 0.4, hjust=0.5))
+b2 <- ggplot(measurements, aes(x=Species, y=Limb.Volume)) + geom_boxplot() + theme_pub() + theme(axis.text.x = element_text(angle = 25, vjust = 0.4, hjust=0.5)); b2
 
 #Create a multi-panel plot showing frequency histograms for each species. 
 require(gridExtra)
