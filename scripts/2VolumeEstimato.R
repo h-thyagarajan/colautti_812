17c9ce3f2004034a173a9327ba5228176bcfcabe
h-thyagarajan <harshavardhanthyagarajan@gmail.com>
2023-01-10 12:57:18 -0500

    Commit 3, fixing VolumeEstimato

--- scripts/2VolumeEstimato.R
+++ scripts/2VolumeEstimato.R
@@ -2,13 +2,11 @@
 measurements <- read.csv(file = "/Users/ht/0/Stats/2 Colautti_812/trial_repo/measurements.csv")
 
 #Getting rid of the extra indexing column, when the file is recalled
-if (names(measurements)[1] == "X") 
-  {measurements <- measurements[,-1]}
+if (names(measurements)[1] == "X") {measurements <- measurements[,-1]}
 
 #Then estimate a limb volume from the two limb measurements. 
-volume <- function(l,w)
-  {v <- pi*w^2*l; return(v)}
+volume <- function(l,w) {v <- pi*(w/2)^2*l; return(v)}
 
 #Over-write the original measurements.csv file so that it now contains a Volume column.
 measurements$Limb.Volume <- volume(measurements$Limb.Length, measurements$Limb.Width)
-write.csv(measurements, file="/Users/ht/0/Stats/2 Colautti_812/trial_repo/measurements.csv")
+write.csv(measurements, file = "/Users/ht/0/Stats/2 Colautti_812/trial_repo/measurements.csv")
